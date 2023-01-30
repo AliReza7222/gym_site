9667cce7d2906d21864f8c42eace8315685bae50
AliReza <AliReza@as-MacBook-Air.local>
2023-01-30 11:55:39 +0330

    create a custom adminpanel for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import MyUser
 
-# Register your models here.
+
+@admin.register(MyUser)
+class AdminPanelMyUser(admin.ModelAdmin):
+    list_filter = [
+        'username',
+    ]
+    fields = [
+        ('username', 'password'),
+        'email',
+        ('first_name', 'last_name'),
+        ('is_active', 'is_staff', 'is_superuser'),
+        'groups',
+        'user_permissions'
+    ]
+    filter_horizontal = ['groups', 'user_permissions']
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+    list_editable = ['is_staff', 'is_active']
