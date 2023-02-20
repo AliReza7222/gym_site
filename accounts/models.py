72e5551911918de80cd7f7511fc7c6d7d9b63a0a
AliReza7222 <ali.r.salehi99@gmail.com>
2023-02-20 21:56:09 +0330

    field password user changed (max_lengh)

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AbstractUser
 class MyUser(AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=15, validators=[check_password])
+    password = models.CharField(max_length=100, validators=[check_password])
     type_user = models.CharField(max_length=1, null=True, blank=True)
 
     def __str__(self):
