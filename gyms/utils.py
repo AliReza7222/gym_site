3802b3ded7f558b765888b329423bcb0cac17061
AliReza7222 <ali.r.salehi99@gmail.com>
2023-04-11 04:14:59 +0430

    fixed utils.py for get location tag a

--- gyms/utils.py
+++ gyms/utils.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 from bs4 import BeautifulSoup
@@ -10,18 +11,15 @@ def create_location():
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     result = soup.find_all('span', class_='mw-headline')
-    dict_ans = dict()
+    list_pro = list()
     for tag_span in result[:31]:
         pro = tag_span.find('a', class_='mw-redirect')
-        dict_ans[pro.text.strip('Province').strip()] = []
+        list_pro.append(pro.text.strip('Province').strip())
 
     table_tags = soup.find_all('table', class_='wikitable sortable')
-
-    list_pro = list(dict_ans.keys())
     for index_table, table in enumerate(table_tags):
-        list_city = list()
         cities = table.find_all('a')
         for city in cities:
-            list_city.append(city.text)
-            Locations.objects.create(province=list_pro[index_table], name_city=city.text)
-        dict_ans[list_pro[index_table]] = list_city
+            name_city = city.text
+            if name_city in re.findall('[a-zA-Z-\s]+', name_city):
+                Locations.objects.create(province=list_pro[index_table], name_city=city.text)
