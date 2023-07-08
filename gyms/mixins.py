c8bdf4b985f599232bb0a528cdb8332d7b05462a
AliReza7222 <ali.r.salehi99@gmail.com>
2023-07-08 18:17:29 +0430

    ExistsNotificationCheckMixin created

--- gyms/mixins.py
+++ gyms/mixins.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect, HttpResponseRedirect
 from .models import Gyms
 
 
@@ -69,3 +69,16 @@ class StudentCheckUserMixin:
             return super().dispatch(request, *args, **kwargs)
         messages.error(request, 'You Can Not Enter This Page .')
         return redirect('home')
+
+
+class ExistsNotificationCheckMixin:
+
+    def dispatch(self, request, *args, **kwargs):
+        gym_pk = kwargs.get('gym_pk')
+        gym = Gyms.objects.get(pk=gym_pk)
+        if gym.notification_set.all().exists():
+            return super().dispatch(request, *args, **kwargs)
+
+        message = 'You have no notifications !'
+        messages.error(request, message)
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
